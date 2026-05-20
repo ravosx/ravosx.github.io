0f854a80c5e58faa360490974f2c0f4ab94f3b18
ravosx <vosatkoradko@gmail.com>
2026-05-20 18:22:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let mode = "register";
 
+function toggleMode(){
+    mode = mode === "register" ? "login" : "register";
+
+    document.getElementById("title").innerText =
+        mode === "register" ? "Registrácia" : "Prihlásenie";
+}
+
+function auth(){
+
+    let user = {
+        meno: document.getElementById("meno").value,
+        heslo: document.getElementById("heslo").value,
+        bio: "",
+        image: "",
+        bg: "linear-gradient(to bottom, rgb(41,139,172), lightblue)"
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    window.location.href = "profile.html";
+}
